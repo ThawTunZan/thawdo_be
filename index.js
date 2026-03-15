7fd6c48cc94264baa5a2731625a08bdaf4809f56
ThawTunZan <thawtunzan@gmail.com>
2026-03-16 00:47:25 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    res.json({ message: "Hello from backend!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
